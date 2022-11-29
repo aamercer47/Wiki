664cfc157541037bb5563da832f7d20d0211a16c
Lenovo <2735850369@qq.com>
2022-11-29 21:00:29 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.java.wiki.controller;
 //开发HelloWorld接口
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //返回字符串
 //@Controller  //返回页面
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     //http://127.0.0.1:8880/hello
     ///常见的http请求方式有4种 GET,POST,PUT,DELETE
     // 通过id来访问用户：/user?id=1或/user/1
@@ -20,7 +25,7 @@ public class TestController {
     //@RequestMapping("/hello") //hello接口地址  //RequestMapping支持所有请求方式
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
 
     @PostMapping("/hello/post")
