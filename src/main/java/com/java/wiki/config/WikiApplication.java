30c3a81b8cb821d35db956a3dc37cad3cead09be
Lenovo <2735850369@qq.com>
2023-02-26 20:31:11 +0800

    3-4 mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/java/wiki/config/WikiApplication.java
+++ src/main/java/com/java/wiki/config/WikiApplication.java
@@ -1,17 +1,18 @@
 package com.java.wiki.config;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 @ComponentScan("com.java")
 //@ComponentScan("com.java","com.test")
 @SpringBootApplication
+@MapperScan("com.java.wiki.mapper")
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.java.wiki.controller;
 //开发HelloWorld接口
 
+import com.java.wiki.domain.Test;
+import com.java.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController //返回字符串
 //@Controller  //返回页面
@@ -12,6 +17,9 @@ public class TestController {
 
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     //http://127.0.0.1:8880/hello
     ///常见的http请求方式有4种 GET,POST,PUT,DELETE
@@ -32,5 +40,13 @@ public class TestController {
     public String helloPost(String name){
         return "Hello world! Post, "+name;
     }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+
+        return testService.list();
+    }
+
+
 }
 
