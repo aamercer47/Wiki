f562160b4e18bedf95f7242202347e40a4cd3030
Lenovo <2735850369@qq.com>
2023-03-03 15:27:57 +0800

    3-5 增加demo表列表查询

--- doc/all.sql
+++ doc/all.sql
@@ -1,3 +1,4 @@
+#增加test表
 drop table if exists `test`;
 create table `test`(
                        `id` bigint not null comment 'id',
@@ -7,3 +8,14 @@ create table `test`(
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `test` (id, name, password) values (1, '测试1', 'password');
+
+
+#增加demo表
+drop table if exists `demo`;
+create table `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine =innodb default charset=utf8mb4 comment ='测试';
+
+insert into `demo` (id, name) values (1,'测试');
---
+++ src/main/java/com/java/wiki/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.java.wiki.controller;
+//开发HelloWorld接口
+
+import com.java.wiki.domain.Demo;
+import com.java.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController //返回字符串
+//@Controller  //返回页面
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list(){
+
+        return demoService.list();
+    }
+
+
+}
+
