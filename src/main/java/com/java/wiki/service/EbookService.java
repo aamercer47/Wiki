05ed40bbef20b4f1d2779d6d7f0b41659bf01f28
Lenovo <2735850369@qq.com>
2023-03-04 14:01:26 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/java/wiki/service/EbookService.java
+++ src/main/java/com/java/wiki/service/EbookService.java
@@ -6,11 +6,10 @@ import com.java.wiki.domain.EbookExample;
 import com.java.wiki.mapper.EbookMapper;
 import com.java.wiki.req.EbookReq;
 import com.java.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.java.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,14 +24,18 @@ public class EbookService {
         criteria= criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         //持久层返回List<Eboook>需要转成List<EbookResp>再返回controller
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            ebookResp.setId(ebook.getId());
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList=new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            ebookResp.setId(ebook.getId());
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return list;
     }
 }
