bd77c8b6dae773be8db838cc08a19ff3b9966b2a
Lenovo <2735850369@qq.com>
2023-04-27 17:21:55 +0800

    8-11 增加读取文档内容功能

--- src/main/java/com/java/wiki/service/DocService.java
+++ src/main/java/com/java/wiki/service/DocService.java
@@ -116,4 +116,10 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    public String findContent(Long id) {
+        Content content = contentMapper.selectByPrimaryKey(id);
+
+        return content.getContent();
+    }
 }
