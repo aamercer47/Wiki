cea8b496dae1067bd595d147165e1f06a67f0395
Lenovo <2735850369@qq.com>
2023-04-27 19:41:03 +0800

    8-12 增加文档页面，首页点击电子书时，进到该电子书的文档页面

---
+++ web/src/views/doc.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="doc">
+        <hi>欢迎来到文档页面</hi>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
