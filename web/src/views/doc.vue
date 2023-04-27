cc05c6403f4b73da6d9e9e9daa89e34910d12fb3
Lenovo <2735850369@qq.com>
2023-04-27 21:52:13 +0800

    8-12 打开文档页面，默认选中第一个结点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,6 +9,7 @@
               @select="onSelect"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
+              :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
@@ -33,8 +34,8 @@ export default defineComponent({
     const route = useRoute();
     const docs = ref();
     const html = ref();
-    // const defaultSelectedKeys = ref();
-    // defaultSelectedKeys.value = [];
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
     // // 当前选中的文档
     // const doc = ref();
     // doc.value = {};
@@ -53,31 +54,6 @@ export default defineComponent({
     const level1 = ref(); // 一级文档树，children属性就是二级文档
     level1.value = [];
 
-
-    /**
-     * 数据查询
-     **/
-    const handleQuery = () => {
-      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          docs.value = data.content;
-
-          level1.value = [];
-          level1.value = Tool.array2Tree(docs.value, 0);
-          //
-          // if (Tool.isNotEmpty(level1)) {
-          //   defaultSelectedKeys.value = [level1.value[0].id];
-          //   handleQueryContent(level1.value[0].id);
-          //   // 初始显示文档信息
-          //   doc.value = level1.value[0];
-          // }
-        } else {
-          message.error(data.message);
-        }
-      });
-    };
-
     /**
      * 内容查询
      **/
@@ -91,6 +67,32 @@ export default defineComponent({
         }
       });
     };
+
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          docs.value = data.content;
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+
+          if (Tool.isNotEmpty(level1)) {
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+            // // 初始显示文档信息
+            // doc.value = level1.value[0];
+          }
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
 
     const onSelect = (selectedKeys: any, info: any) => {
       console.log('selected', selectedKeys, info);
@@ -123,7 +125,7 @@ export default defineComponent({
       level1,
       html,
       onSelect,
-      // defaultSelectedKeys,
+      defaultSelectedKeys,
       // doc,
       // vote
     }
