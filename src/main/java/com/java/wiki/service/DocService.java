3753428fe451c8cfd41c1752361f1dc6091a56b1
Lenovo <2735850369@qq.com>
2023-04-28 23:18:45 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/java/wiki/service/DocService.java
+++ src/main/java/com/java/wiki/service/DocService.java
@@ -156,4 +156,8 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo() {
+        docMapperCust.updateEbookInfo();
+    }
 }
