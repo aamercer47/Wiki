b74250096c1b48e685de3685d008df68164d3f94
Lenovo <2735850369@qq.com>
2023-04-28 22:40:58 +0800

    10-3 同一个IP，一天之内只能对同一文档点赞一次

--- src/main/java/com/java/wiki/aspect/LogAspect.java
+++ src/main/java/com/java/wiki/aspect/LogAspect.java
@@ -2,6 +2,7 @@ package com.java.wiki.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import com.java.wiki.util.RequestContext;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
@@ -46,6 +47,7 @@ public class LogAspect {
         LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
         LOG.info("远程地址: {}", request.getRemoteAddr());
 
+        RequestContext.setRemoteAddr(getRemoteIp(request));//获取远程ip并放到线程本地变量
 
         // 打印请求参数
         Object[] args = joinPoint.getArgs();
