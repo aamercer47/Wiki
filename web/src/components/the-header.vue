9872d3dcafaf5c17d2fe1a90d947e269faab783e
Lenovo <2735850369@qq.com>
2023-04-28 17:13:19 +0800

    9-9 前端显示登录昵称，遗留问题，刷新会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,7 +21,10 @@
           <a-menu-item key="/about">
             <router-link to="/about">关于我们</router-link>
           </a-menu-item>
-          <a class="login-menu" @click="showLoginModal">
+          <a class="login-menu" v-show="user.id">
+            <span>您好：{{user.name}}</span>
+          </a>
+          <a class="login-menu" v-show="!user.id" @click="showLoginModal">
             <span>登录</span>
           </a>
         </a-menu>
@@ -57,13 +60,14 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup () {
-    // // 登录后保存
-    // const user = computed(() => store.state.user);
+    // 登录后保存
+    const user = ref();
+    user.value={};
 
     // 用来登录
     const loginUser = ref({
       loginName: "test",
-      password: "test"
+      password: "test123"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -82,7 +86,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-
+          user.value=data.content;
           // store.commit("setUser", data.content);
         } else {
           message.error(data.message);
@@ -110,7 +114,7 @@ export default defineComponent({
       showLoginModal,
       loginUser,
       login,
-      // user,
+      user,
       // logout
     }
   }
