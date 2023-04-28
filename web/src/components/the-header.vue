a6e5ba9b728b5fefb913ee10cce534f7f45e960d
Lenovo <2735850369@qq.com>
2023-04-28 15:40:23 +0800

    9-8 前后端调试成功，完成基本的登录功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -49,7 +49,7 @@
 import { defineComponent, ref, computed } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
-import store from "@/store";
+// import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -74,20 +74,20 @@ export default defineComponent({
     // 登录
     const login = () => {
       console.log("开始登录");
-      // loginModalLoading.value = true;
-      // loginUser.value.password = hexMd5(loginUser.value.password + KEY);
-      // axios.post('/user/login', loginUser.value).then((response) => {
-      //   loginModalLoading.value = false;
-      //   const data = response.data;
-      //   if (data.success) {
-      //     loginModalVisible.value = false;
-      //     message.success("登录成功！");
-      //
-      //     store.commit("setUser", data.content);
-      //   } else {
-      //     message.error(data.message);
-      //   }
-      // });
+      loginModalLoading.value = true;
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post('/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("登录成功！");
+
+          // store.commit("setUser", data.content);
+        } else {
+          message.error(data.message);
+        }
+      });
     };
 
     // // 退出登录
